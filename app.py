0bda23a53af449751852785e93105eac79b5f5c7
faestartsolutionsrei-max <fae.startsolutionsrei@gmail.com>
2026-04-04 01:59:52 +0800

    app.py

--- app.py
+++ app.py
@@ -83,4 +83,5 @@ def health_check():
     return jsonify({'status': 'running'}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+port = int(os.environ.get('PORT', 8080))
+app.run(host='0.0.0.0', port=port)
